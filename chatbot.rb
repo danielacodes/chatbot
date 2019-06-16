1a0843b837f2282ae3ef4eb34b38860f7fec2d19
Daniela <danielavitosova@seznam.cz>
2019-06-16 17:37:38 +0200

    Adding more features

--- chatbot.rb
+++ chatbot.rb
@@ -13,6 +13,8 @@ socket.puts "JOIN #{channel}"
 
 socket.puts "PRIVMSG #{channel} :I am so happy to be here!"
 
+quotes = ["It is during our darkest moments that we must focus to see the light. - Aristotle", "The way get started is to quit talking and being doing. - Walt Disney", "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty. - Winston Churchill"]
+
 while message = socket.gets do
   puts message
 
@@ -20,6 +22,9 @@ while message = socket.gets do
     server = message.split(':').last
     puts "PONG #{server}"
     socket.puts "PONG #{server}"
+  elsif message.match('Oh Bot, I feel down. Inspire me!')
+    puts "PRIVMSG #{channel} :#{quotes.sample(1)}"
+    socket.puts "PRIVMSG #{channel} :#{quotes.sample(1)}"
   elsif message.match('How are you?')
     puts "PRIVMSG #{channel} :I'm great. Thanks. How are you?"
     socket.puts "PRIVMSG #{channel} :I'm great. Thanks. How are you?"
@@ -29,5 +34,13 @@ while message = socket.gets do
   elsif message.match('It is nice to meet you :D')
     puts "PRIVMSG #{channel} :Nice to meet you too!"
     socket.puts "PRIVMSG #{channel} :Nice to meet you too!"
+  elsif message.match('I want to be your BFF!')
+    puts "PRIVMSG #{channel} :I want to be yours too"
+    socket.puts "PRIVMSG #{channel} :I want to be yours too"
+    File.write('brain.txt', "Daniela")
+  elsif message.match('Who is your BFF?')
+    BFF = File.read('brain.txt')
+    puts "PRIVMSG #{channel} :#{BFF}"
+    socket.puts "PRIVMSG #{channel} :#{BFF}"
   end
 end
